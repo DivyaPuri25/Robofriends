4d1ad30ca387dba8103e79dd7e9d4cdec3f71a26
Divya Puri <dvy.puri@gmail.com>
2019-11-03 14:24:13 -0500

    adding project files

---
+++ build/precache-manifest.753c0534a2807d68ceeaf0523d718fbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfddb41caf73a869699faf6ef7d5d1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a19d605f56d48d1736d",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "04e094724c4967010337",
+    "url": "/static/css/main.12cbb123.chunk.css"
+  },
+  {
+    "revision": "7a19d605f56d48d1736d",
+    "url": "/static/js/2.5113658f.chunk.js"
+  },
+  {
+    "revision": "04e094724c4967010337",
+    "url": "/static/js/main.1c13805d.chunk.js"
+  },
+  {
+    "revision": "f342ecb9eb1f9a22b13f",
+    "url": "/static/js/runtime-main.cedaa9fa.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
