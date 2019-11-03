ff7d433d190ba5a56888ba17bb2f2e7b4a960f62
Divya Puri <dvy.puri@gmail.com>
2019-11-03 15:22:41 -0500

    Updates

---
+++ precache-manifest.ea37047b5f086225ec0392930ca97cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289fba595fed9a5c8cc7068b34b5b044",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "d7179ab0a3198a429a86",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fe3a9e65360548888ed9",
+    "url": "/Robofriends/static/css/main.e52952e1.chunk.css"
+  },
+  {
+    "revision": "d7179ab0a3198a429a86",
+    "url": "/Robofriends/static/js/2.9aeb588b.chunk.js"
+  },
+  {
+    "revision": "fe3a9e65360548888ed9",
+    "url": "/Robofriends/static/js/main.ae789c21.chunk.js"
+  },
+  {
+    "revision": "7d345c0f0e98a388c01b",
+    "url": "/Robofriends/static/js/runtime-main.22fd3be1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
